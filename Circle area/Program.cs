92fc19c846ee168e826298f56d7669b7f309f4a1
Tommy1938Angelo <Tommy1938@ya.ru>
2021-05-13 22:12:09 +0300

    Добавьте файлы проекта.

---
+++ Circle area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Circle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                const double pi = 3.14;
+                double radius;
+                Console.Write("Enter the radius value: ");
+                radius = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Circle_area =" + pi * radius * radius);
+                Console.WriteLine("==========================================");
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
